69d5aec7f936259cbab09869f69ccde3414f07e2
Masaki Yoshiiwa <yopinoji@yahoo.co.jp>
2020-07-30 02:43:15 +0900

    first commit

---
+++ ABC086A/index.js
@@ -0,0 +1,10 @@
+function main(input) {
+  input = input.split("\n");
+  const tmp = input[0].split(" ");
+  const OddOrEven = (tmp[0] * tmp[1]) % 2;
+  const result = !!OddOrEven ? "Odd" : "Even";
+  //出力
+  console.log("%s", result);
+}
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+main(require("fs").readFileSync("/dev/stdin", "utf8"));
---
+++ README.md
@@ -0,0 +1,16 @@
+# AtCoder Environment for JavaScript
+
+## Create template.
+
+Run following(`ABC086A` is sample):
+
+```bash
+$ npm run new ABC086A
+```
+
+## Test function.
+
+```bash
+$ cd ABC086A
+$ node index.js < input.txt
+```
